e4fcfd045dec2c5167e545e2bff4e055c213fa0b
symoh <smnkimathi@gmail.com>
2021-01-27 12:35:45 +0300

    converted user model to an entity

--- src/main/java/com/tutorial/stripes/model/User.java
+++ src/main/java/com/tutorial/stripes/model/User.java
@@ -1,22 +1,37 @@
 package com.tutorial.stripes.model;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.*;
 
 @Getter
 @Setter
+@NoArgsConstructor
+@ToString
+@Entity
 public class User {
 
+    @Column
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private String id;
 
+    @Column
     private String firstName;
 
+    @Column
     private String lastName;
 
+    @Column
     private String surname;
 
+    @Column
     private String username;
 
+    @Column
     private String password;
 
 
